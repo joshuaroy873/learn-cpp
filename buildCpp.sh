d2ec33f20f62de6bff204f7d647b9d5657dde518
joshuaroy873 <joshuaroy873@gmail.com>
2025-11-14 22:39:33 -0500

    Added buildCpp.sh script to build, run and delete cpp files.

---
+++ buildCpp.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Check if an input file is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <input_file.cpp>"
+    exit 1
+fi
+
+INPUT_FILE="$1"
+EXECUTABLE_NAME="${INPUT_FILE%.cpp}" # Removes .cpp extension to get executable name
+
+# Compile the C++ program
+echo "Compiling $INPUT_FILE..."
+g++ "$INPUT_FILE" -o "$EXECUTABLE_NAME"
+
+# Check if compilation was successful
+if [ $? -eq 0 ]; then
+    echo "Compilation successful. Running $EXECUTABLE_NAME..."
+    # Run the compiled program
+    ./"$EXECUTABLE_NAME"
+
+    # Delete executable after running
+    if [ -f "$EXECUTABLE_NAME" ]; then
+        echo "Cleaning up: removing executable $EXECUTABLE_NAME"
+        rm "$EXECUTABLE_NAME"
+    fi
+    
+else
+    echo "Compilation failed."
+fi
